1cfc1791d4a0a28935d333eb64bbcf386aa9815d
robertbounds <robert.s.bounds@gmail.com>
2014-06-16 01:12:39 -0500

    more intermediate changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,44 +1,55 @@
 run_analysis <- function() {
         
-        
-        ## Directory navigation, read test & train data for both X and y
+        ## Directory navigation & file reading block
+        ##
+        ## This chunk of code navigates the UCI HAR Dataset directory,
+        ## and reads necessary files along the way.
+        ##
+        ## dataset home
         setwd("UCI HAR Dataset")
         datasetHome <- getwd()
+        features    <- read.table("features.txt")
+        ## test directory
         setwd("test")
-        xTest <- read.table("X_test.txt")
-        yTest <- read.table("y_test.txt")
+        xTest       <- read.table("X_test.txt")
+        yTest       <- read.table("y_test.txt")
+        subjectTest <- read.table("subject_test.txt")
         setwd(datasetHome)
+        ## train directory
         setwd("train")
-        xTrain <- read.table("X_train.txt")
-        yTrain <- read.table("y_train.txt")
+        xTrain       <- read.table("X_train.txt")
+        yTrain       <- read.table("y_train.txt")
+        subjectTrain <- read.table('subject_train.txt')
         setwd(datasetHome)
         
-        ## read activity labels, and x-variable names
-        features <- read.table("features.txt")
-        features <- features[, 2]
-        activityLabels <- read.table("activity_labels.txt")
+        ## initial merges of data frames
+        xData       <- rbind(xTest, xTrain)
+        yData       <- rbind(yTest, yTrain)
+        subjectData <- rbind(subjectTest, subjectTrain)
         
-        ## merge data frames
-        xData <- rbind(xTest, xTrain)
-        yData <- rbind(yTest, yTrain)
+        ## rename variables for data frames
+        names(xData)       <- features
+        names(yData)       <- "ACTIVITY"
+        names(subjectData) <- "SUBJECT ID"
         
-        ## update x-variable names
-        names(xData) <- features
-        
-        ## update activity labels
-        names(yData) <- "ACTIVITY NAMES"
-        #for (i in 1:6) {
-        #        yData[yData == i] <- activityLabels[i]
-        #}
-        
-        ## keys/values determined by activity_labels.txt
+        ## rename observation data for activities
+        ## keys/values determined by looking at activity_labels.txt
         yData[yData == 1] <- "WALKING"
         yData[yData == 2] <- "WALKING_UPSTAIRS"
         yData[yData == 3] <- "WALKING_DOWNSTAIRS"
         yData[yData == 4] <- "SITTING"
         yData[yData == 5] <- "STANDING"
         yData[yData == 6] <- "LAYING"
-
         
+        ## glue subject id and activity names to front of dataset
+        fullData <- cbind(yData, xData)
+        fullData <- cbind(subjectData, fullData)
         
+        #######################################################################
+        ###  write code here to subset fullData
+        #######################################################################
+        
+        #######################################################################
+        ###  Write code here to construct tidy data set
+        #######################################################################
 }
